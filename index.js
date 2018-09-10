43e12d24592a95ff457d3ee9c93ef968c28ea6db
Steve Cuzzort <steve.cuzzort@gtri.gatech.edu>
2018-09-09 22:11:44 -0400

    fixed calls to work with new aws inspired paridim

--- index.js
+++ index.js
@@ -1,38 +1,57 @@
 const RouteStackLayer = require("./RouteStackLayer").inject();
 
+var _instanceOfServless = null;
+
+function Servless(config) {
+    this.config = config;
+    this.AwsPolicies = require("./AwsPolicies").inject(this.config);
+
+    this.endPoints = [];
+    this.enviornmentVariables = [];
+    this.root = null;
+};
+
+Servless.prototype.route = function(path) {
+    this.root = RouteStackLayer.route(path);
+    return this.root;
+};
+
+Servless.prototype.getPolicies = function() {
+    return this.AwsPolicies.getPolicies(this.config);
+};
+
+Servless.prototype.getRoot = function() {
+    return this.root
+};
+
+exports.getCurrentInstance = function(){
+    return _instanceOfServless;
+};
+
+exports.handleCall = function(event, context, callback) {
+    console.log("here");
+    console.log(JSON.stringify(event, null, 4));
+    console.log(JSON.stringify(context, null, 4));
+    _instanceOfServless.getPolicies()
+        .then( policies => {
+            callback(null, {StatusCode:200, Body:JSON.stringify({
+                    "policies": policies
+                })
+            });
+        });
+    };
+
+exports.initWithConfig = function (_options) {
+    _instanceOfServless = new Servless(_options);
+    return _instanceOfServless
+};
+
+
+module.exports = exports;
+/*
 module.exports = function(_options){
 
-    function handleCall(event, context) {
-        console.log("inside the app for realz");
-        return {
-            StatusCode: 200,
-            Body: JSON.stringify({
-                message: 'Go Serverless v1.0! Your function executed successfully!'
-            }),
-        }
-    };
 
-    function Servless(config) {
-        this.config = config;
-        this.AwsPolicies = require("./AwsPolicies").inject(this.config);
-
-        this.endPoints = [];
-        this.enviornmentVariables = [];
-        this.root = null;
-    };
-
-    Servless.prototype.route = function(path) {
-        this.root = RouteStackLayer.route(path);
-        return this.root;
-    };
-
-    Servless.prototype.getPolicies = function() {
-        return this.AwsPolicies.getPolicies(this.config);
-    };
-
-    Servless.prototype.getRoot = function(){
-        return this.root;
-    };
 
     return new Servless(_options);
-};
+};*/
